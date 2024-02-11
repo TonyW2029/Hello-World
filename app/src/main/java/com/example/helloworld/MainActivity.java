6ceafe07b7bbcee3141133e9e916bc96215ae1ac
Anthony <Williamsa55@montclair.edu>
2024-02-11 14:44:08 +0530

    Completed task

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+        textView.setText("This is a simple hello world app.");
+    }
+}
